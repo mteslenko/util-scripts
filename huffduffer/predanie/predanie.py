35b6670fc5e2a6823990d727f1a8155735a45967
Mikhail Teslenko <m.teslenko@gmail.com>
2017-06-10 13:31:52 -0700

    add constants

--- huffduffer/predanie/predanie.py
+++ huffduffer/predanie/predanie.py
@@ -3,11 +3,13 @@ from urllib.parse import quote
 
 from pyquery import PyQuery
 
+HTML_FILE_NAME = 'predanie.html'
+HUFFDUFFER_TAG = 'Christianity'
 HUFFDUFFER_ADD_URL = 'https://huffduffer.com/add?bookmark%5Burl%5D={0}&bookmark%5Btitle%5D={1}&bookmark%5Bdescription%5D={2}&bookmark%5Btags%5D={3}'
 
 
 def main():
-    d = PyQuery(filename='predanie.html')
+    d = PyQuery(filename=HTML_FILE_NAME)
     description = d('.forums-section__subtitle')[0].text
     tracks = d('li.track')
     for el in [x for x in tracks if x.get('ng-init')]:
@@ -15,7 +17,7 @@ def main():
         url = re.search('src: \'(.+)\'', el.get('ng-init')).group(1)
         print(title)
         huffduffer_url = HUFFDUFFER_ADD_URL.format(url, quote(title), quote(description),
-                                                   'Christianity')
+                                                   HUFFDUFFER_TAG)
         print(huffduffer_url)
 
 
