9deae29456d9a752e2d252874672dc872e85b7aa
Mikhail Teslenko <m.teslenko@gmail.com>
2017-06-10 13:28:59 -0700

    predanie.ru

---
+++ huffduffer/predanie/predanie.py
@@ -0,0 +1,23 @@
+import re
+from urllib.parse import quote
+
+from pyquery import PyQuery
+
+HUFFDUFFER_ADD_URL = 'https://huffduffer.com/add?bookmark%5Burl%5D={0}&bookmark%5Btitle%5D={1}&bookmark%5Bdescription%5D={2}&bookmark%5Btags%5D={3}'
+
+
+def main():
+    d = PyQuery(filename='predanie.html')
+    description = d('.forums-section__subtitle')[0].text
+    tracks = d('li.track')
+    for el in [x for x in tracks if x.get('ng-init')]:
+        title = re.search('(.+) \(', el[0][2].text.strip()).group(1)
+        url = re.search('src: \'(.+)\'', el.get('ng-init')).group(1)
+        print(title)
+        huffduffer_url = HUFFDUFFER_ADD_URL.format(url, quote(title), quote(description),
+                                                   'Christianity')
+        print(huffduffer_url)
+
+
+if __name__ == '__main__':
+    main()
